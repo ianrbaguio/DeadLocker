e9257ef565f08051737a59d11bb6a5bc543fe021
ianrbaguio <ianrey_2004@yahoo.com>
2020-10-18 15:10:39 -0600

    Add files via upload

---
+++ DeadLocker.ino
@@ -0,0 +1,30 @@
+/*
+ * Name: DeadLocker application
+ * Date Created: October 18, 2020
+ * Creators: Jester Cornejo & Ian Baguio
+ * Date Updated: October 18, 2020
+ * 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+/*
+ * Function: deadLocker
+ * 
+ * Parameters: status - true = lock
+ *                      false = unlock
+ * 
+ */
+void deadLocker(bool status){
+  
+}
